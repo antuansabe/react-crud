02d16e8f1cb2dc78d4b2854b400d965896b5d88d
Antonio DroCode <antuansabe@gmail.com>
2021-10-11 12:52:32 -0500

    config the proyect to start coding

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Fragment } from 'react/cjs/react.production.min';
+
+function App() {
+  return (
+      <Fragment>
+
+
+
+      </Fragment>
+  );
+}
+
+export default App;
