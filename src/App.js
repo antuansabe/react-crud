4a8a142359c5d034f9041b7bf1c873030a6a9c44
Antonio DroCode <antuansabe@gmail.com>
2021-10-11 14:04:40 -0500

    feature(app.js) import libraries and made main buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
 
-import './App.css';
+import react from 'react';
 import { Fragment } from 'react/cjs/react.production.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table,Button,Container,Modal, ModalBody,ModalHeader,FormGroup,ModalFooter } from 'reactstrap';
+
+
+const data= [
+    { id:1, personaje: 'Naruto', anime: 'Naruto'},
+    { id:2, personaje: 'Nezuco', anime: 'Demon Slayer'},
+    { id:3, personaje: 'Goku', anime: 'Dragon Ball'},
+    { id:4, personaje: 'Kenshin', anime: 'Roruouni'},
+    { id:5, personaje: 'Monkey D. Luffy', anime: 'One Piece'},
+    { id:6, personaje: 'Seto Kaiba', anime: 'Yu-Gi-Oh!'},
+];
+
 
 function App() {
+
+  state = {
+    data: data,
+    modalActualizar: false,
+    modalInsertar: false,
+    form: {
+      id: "",
+      personaje: "",
+      anime: "",
+    },
+  };
   return (
       <Fragment>
+        <Container> 
+          <br/>
+            <Button color="primary">Insertar Nuevo Personaje</Button>
+          <br/> 
+
+          <Table>
+            <thead><tr><th>Id</th>
+            <th>Personaje</th>
+            <th>Anime</th>
+            <th>Acciones</th>
+            </tr></thead>
+
+            <tbody>
+              {this.state.data.map( (elemento) =>{
+                <tr>
+                  <td>{elemento.id}</td>
+                  <td>{elemento.personaje}</td>
+                  <td>{elemento.anime}</td>
+                  <td><Button color="primary">Editar </Button></td>
+                  <td><Button color="danger">Eliminar </Button></td>
+                </tr>
+              } )}
+            </tbody>
+              
+          </Table>
 
 
 
+        </Container>
       </Fragment>
   );
 }
